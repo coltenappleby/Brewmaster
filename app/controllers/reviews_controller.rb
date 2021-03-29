94bb11e0f3846f67402547a42108809be7e9e2a5
Jabair <jabairk@gmail.com>
2021-03-29 16:01:18 -0400

    Added reviews page, linked to beers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,6 @@
 class ReviewsController < ApplicationController
+    
+    def show
+        @review = Review.find(params[:id])
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,18 @@ class Review < ApplicationRecord
     belongs_to :user 
     belongs_to :beer 
     belongs_to :bar 
+
+    def find_author
+        User.where(id: self.user_id).first.username
+    end
+
+    def find_bar
+        Bar.where(id: self.bar_id).first.name
+    end
+
+    def find_beer
+        Beer.where(id: self.beer_id).first.name 
+    end
         
    
 end
