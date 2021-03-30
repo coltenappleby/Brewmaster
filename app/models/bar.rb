f2b3c7665cb2b2c2b84ba2048e769b9f0903bf99
coltenappleby <colten.appleby@gmail.com>
2021-03-30 00:47:12 -0400

    created beer menu method in Bar model

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -4,9 +4,20 @@ class Bar < ApplicationRecord
     has_many :users, through: :reviews 
     has_many :beer_menus
 
-    # def beer_list
-    #     Beer.where(id: @beer.id)
-    # end
+    def beer_list
+        beers = []
+        self.beer_menus.each do |relationship|
+            beers << Beer.find(relationship.beer_id)
+        end
+        return beers
+
+        # beer_ids = []
+        # self.beer_menus.each do |relationship|
+        #     beer_ids << relationship.beer_id
+        # end
+        # Beer.find(beer_ids)
+    end
+
 # #in html 
 #     2.6.1 :002 > Bar.first.beers.each do |beer|
 #         2.6.1 :003 >     beer.name
