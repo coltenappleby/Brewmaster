39e3dc7791d19a62bdf3536a16e9acdfe8f08e5a
Jabair <jabairk@gmail.com>
2021-03-30 11:50:47 -0400

    Created Beer

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,6 +5,30 @@ class BeersController < ApplicationController
     end
 
     def show
+        find_beer 
+    end
+
+    def new
+        beer_styles 
+        @beer = Beer.new
+    end
+    
+    def create
+        @beer = Beer.create(beer_params(:name, :style, :brewery_id, :alocohol ))
+        redirect_to beer_path(@beer)
+    end
+
+    private
+
+    def find_beer
         @beer = Beer.find(params[:id])
+    end
+
+    def beer_params(*args)
+        params.require(:beer).permit(*args)
+    end
+
+    def beer_styles
+        @styles = ["lager", "IPA", "Stout", "NE IPA", "Cider", "Sour", "Porter"]
     end
 end
