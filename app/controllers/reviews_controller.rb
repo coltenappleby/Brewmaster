b00f70b42dafecf8912652d696333d595e758347
Jabair <jabairk@gmail.com>
2021-03-30 10:43:04 -0400

    added create for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,26 @@ class ReviewsController < ApplicationController
     def show
         @review = Review.find(params[:id])
     end
+
+    def new 
+        @review = Review.new
+
+        @users = User.all 
+        @bars = Bar.all
+        @beers = Beer.all 
+
+    end
+
+    def create
+        @review = Review.create(review_params(:user_id, :title, :content, :beer_id, :bar_id, :rating)) 
+
+        redirect_to review_path(@review.id)
+        #come back to deal with validations
+    end
+
+    private 
+
+    def review_params(*args)
+        params.require(:review).permit(*args)
+    end
 end
