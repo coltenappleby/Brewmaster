01b10779772f484b7625b4608c09043f67d9f788
coltenappleby <colten.appleby@gmail.com>
2021-03-31 15:54:02 -0400

    created authorization of user before review creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
     def logged_in_user
         @current_user = User.find_by(id: cookies[:user_id])
     end
+
+    def logged_in?
+        !logged_in_user.nil?
+    end
+
+    def authorized
+        redirect_to login_path unless logged_in?
+    end
 end
